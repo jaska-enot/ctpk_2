d3bd7bfd36c2526b61116ab077110e8d19bf48a6
jaska-enot <bogania@yandex.ru>
2023-12-13 01:27:20 +0300

    some assets and animation controller for Chelik-object

---
+++ Assets/Scripts/ChelikController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChelikController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private const string HorizontalAxisName = "Horizontal";
+    private float _hAxisValue;
+    private static readonly int Speed = Animator.StringToHash("speed");
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        _hAxisValue = Input.GetAxis(HorizontalAxisName);
+        _animator.SetInteger(Speed, (int)_hAxisValue);
+        _rigidbody.AddForce(new Vector2(_hAxisValue*2, 0.0f));
+
+
+            
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(new Vector2(0.0f, 10.0f));
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+        }
+    }
+}
